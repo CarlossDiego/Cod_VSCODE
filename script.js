7644f883a3d8be19c826bd9dba940f801ac021a0
CarlossDiego <carlos.diego08765@gmail.com>
2025-11-19 20:30:31 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,20 @@
+function mostrarPopupB() {
+    var popup = document.getElementById('popupB');
+    popup.style.display = 'flex';
+}
+
+function fecharPopupB() {
+    var popup = document.getElementById('popupB');
+    popup.style.display = 'none';
+}
+
+
+function mostrarPopupC() {
+    var popup = document.getElementById('popupC');
+    popup.style.display = 'flex';
+}
+
+function fecharPopupC() {
+    var popup = document.getElementById('popupC');
+    popup.style.display = 'none';
+}
